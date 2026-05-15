847fe115121df34bc20b63c3420a1a4e42084270
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-05-15 16:51:31 -0400

    add some logging

--- stellar-main/datasets.py
+++ stellar-main/datasets.py
@@ -52,6 +52,8 @@ def load_hubmap_data(
     print("Common variables:", common_vars)
 
     test_adata = test_adata_full[:, common_vars].copy()
+    print(test_adata.obsm_keys())
+    print(test_adata.var_names)
     unlabeled_pos = test_adata.obsm["X_spatial"]
     unlabeled_regions = test_adata.obs["unique_region"]
 
