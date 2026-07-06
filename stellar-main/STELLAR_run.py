84431fbcfa29c9e6868b8d219f7cacc8333c3be8
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-07-06 11:30:43 -0400

    add logging

--- stellar-main/STELLAR_run.py
+++ stellar-main/STELLAR_run.py
@@ -173,6 +173,7 @@ def main():
         out_dir = Path("stellar")
         out_dir.mkdir(exist_ok=True, parents=True)
         print(results)
+        print(unlabeled_cell_indexes)
         idxs, annotations = zip(unlabeled_cell_indexes, results)
         predictions_df = pd.DataFrame({'ID': idxs,
                                       'STELLAR_CellType': annotations})
