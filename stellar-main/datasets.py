549e46b90fbeeb7ba3cfc2d662f31b2f890f507e
pennycuda <141153875+pennycuda@users.noreply.github.com>
2026-07-06 11:20:30 -0400

    fix this

--- stellar-main/datasets.py
+++ stellar-main/datasets.py
@@ -86,14 +86,12 @@ def load_hubmap_data(
     train_adata_full = train_adata_full[train_adata_full.obs['cell_type'] != 'Noise']
     print(train_adata_full)
     print(train_adata_full.obs)
-    train_var = [v.lower() for v in list(train_adata_full.var_names)]
     print("Training data variables:", train_adata_full.var_names)
 
     test_adata_full = anndata.read_h5ad(unlabeled_file)
     print("Test data variables:", test_adata_full.var_names)
-    test_var = [v.lower() for v in list(test_adata_full.var_names)]
     # Markers must all match and be in the same order
-    common_vars = [v for v in train_var if v in test_var]
+    common_vars = [v for v in train_adata_full.var_names if v in test_adata_full.var_names]
     print("Common variables (Training Order):", common_vars)
     test_adata = test_adata_full[:, common_vars].copy()
 
